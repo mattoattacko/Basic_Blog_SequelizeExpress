fdef25f2aa980c14ba00e1d695d8804d2f3e0071
Matto <matt.mcquain@gmail.com>
2020-01-31 20:37:41 -0800

    connected to server

--- models/article.js
+++ models/article.js
@@ -4,6 +4,9 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   class Article extends Sequelize.Model {}
   Article.init({
+    title: Sequelize.STRING,
+    author: Sequelize.STRING,
+    body: Sequelize.TEXT
 
   },  { sequelize });
 
