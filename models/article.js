6e3cdd373c0826fdb76c3aa26fbd74ae39f7033f
Matto <matt.mcquain@gmail.com>
2020-02-02 22:53:39 -0800

    testing short description method

--- models/article.js
+++ models/article.js
@@ -4,9 +4,13 @@ const moment = require('moment');
 
 module.exports = (sequelize) => {
   class Article extends Sequelize.Model {
-    publishedAt() {
+    publishedAt() { // this next code is "inside the method"
       const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
       return date;
+    }
+    shortDescription() {
+      const shortDesc = this.body.length > 200 ? this.body.substring(0,200) + '...' : this.body;
+      return shortDesc;
     }
   }
   Article.init({
